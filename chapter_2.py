8cf7acdd2a5e8ac386d4681cac47aaca1ee5b3d6
Yug <yugjain1646@gmail.com>
2026-08-18 16:10:08 +0530

    Operators in Python

---
+++ chapter_2.py
@@ -0,0 +1,26 @@
+
+#! ----- Chapter 2 - Math & Operators -----
+
+friends = 5
+
+#? Arithmetic Operators
+
+friends = friends + 1   #* Addition
+friends = friends - 1   #* Subtraction
+friends = friends * 1   #* Multiplication
+friends = friends / 1   #* Division
+friends = friends % 1   #* Modules (Remainder)
+friends = friends ** 2   #* Exponent
+friends = friends // 2   #* Floor Division
+
+#? Augmented Operators
+
+friends +=  1   #* Addition
+friends -=  1   #* Subtraction
+friends *=  1   #* Multiplication
+friends /=  1   #* Division
+friends %=  1   #* Modules (Remainder)
+friends **=  2   #* Exponent
+friends //=  2   #* Floor Division
+
+print(f"You have {friends} friends")
---
+++ projects/1_madlibsGame.py
@@ -0,0 +1,14 @@
+# Madlibs Game
+# Word game where you create a story
+
+shopName = input("Enter shop name: ")
+foodItem = input("Enter food Item: ")
+adjective1 = input("Enter adjective: ")
+price = float(input("Enter price: "))
+place = input("Enter place: ")
+
+print(f"Today I went to {shopName} to try thier new {foodItem}")
+print(f"I love {foodItem}, because there are {adjective1}")
+print(f"I gave the order and was waiting for {adjective1} {foodItem}")
+print(f"I enjoyed it and paid ${price}")
+print(f"Then I went to {place}")

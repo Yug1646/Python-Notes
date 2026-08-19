1f86eec5256f6b9254d7b8b1ffb48e994a68fc7b
Yug <yugjain1646@gmail.com>
2026-08-19 12:17:42 +0530

    Completed Chapter 2 Math & Operators

--- chapter_2.py
+++ chapter_2.py
@@ -1,3 +1,4 @@
+import math
 
 #! ----- Chapter 2 - Math & Operators -----
 
@@ -24,3 +25,20 @@ friends **=  2   #* Exponent
 friends //=  2   #* Floor Division
 
 print(f"You have {friends} friends")
+
+#? Math Functions
+x = 3.14
+y = -4
+z = 5
+
+result = round(x)   #* Round Function       
+result = abs(y)     #* ABS Function    
+result = pow(4,2)   #* Power Function
+result = max(x,y,z) #* Maximum Function
+result = min(x,y,z) #* Minimum Function
+
+print(math.pi)          #* Returns the PI value
+result = math.sqrt(4)   #* Returns the square root of number
+result = math.ceil(4.2) #* rounds a number UPWARD to the nearest integer
+result = math.floor(4.9) #* rounds a number Down to the nearest integer
+print(result)

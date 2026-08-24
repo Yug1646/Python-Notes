75e2df841ca64942d1d6db1eac082ff772c84fe2
Yug <yugjain1646@gmail.com>
2026-08-24 21:36:00 +0530

    String methods & indexing

--- chapter_1.py
+++ chapter_1.py
@@ -7,6 +7,65 @@ print(f"{firstName}'s favourite food is {food}")
 
 print(type(firstName))
 
+#? --- String Methods 
+
+#* len - Returns the Length of the string
+result = len(firstName)
+
+#* find - Used to find a character in string (Returns Integer)
+result = firstName.find("u")
+
+#* rfind - Used to find a character in string from the end (Returns Integer)
+result = firstName.find("g")
+
+#* capitalize - Used to capitialize the first character
+result = firstName.capitalize()
+
+#* upper - Used to capitialize the whole string
+result = firstName.upper()
+
+#* lower - Used to lower the whole string
+result = firstName.lower()
+
+#* isdigit - Used to check if string has numbers (Return Boolean)
+result = firstName.isdigit()
+
+#* isalpha - Used to check if string has alphabets (Returns Boolean)
+result = firstName.isalpha()
+
+#* count - Used to count a character
+result = firstName.count("-")
+
+#* replace - Used to replace a character
+result = firstName.replace("u","o")
+
+#? --- String Indexing
+
+credit_number = "1234-5678-1646-2006"
+last_digit = credit_number[-4:]
+rev_credit = credit_number[::-1]
+
+#* Number at specific index
+print(credit_number[0])
+
+#* Number from start to end 
+print(credit_number[0:4])
+
+#* Starting from 0 till the specified index 
+print(credit_number[:9])
+
+#* Everything upto the end
+print(credit_number[5:]) 
+
+#* Using negative index
+print(credit_number[-1])
+
+#* Using Step
+print(credit_number[::3]) 
+
+print(f"XXXX-XXXX-{last_digit}")
+print(rev_credit)
+
 #? Integer
 age = 20
 print(f"Age of {firstName} is {age}")
--- main.py
+++
@@ -1,6 +0,0 @@
-age = 20
-has_id = True
-is_banned = False
-
-if age < 18 or is_banned:
-    print("You cannot enter")

0c7defdba71e8be9dc5599dff43f12ee96cc8644
Yug <yugjain1646@gmail.com>
2026-08-29 11:13:29 +0530

    List Complete

--- chapter_5.py
+++ chapter_5.py
@@ -2,5 +2,51 @@
 #! ----- Chapter 5 - Data Structures: List, Sets, Tuples, Dictionaries -----
 
 #? List: []
+fruits = ["Apple","Apple","Banana","Mango","Watermelon","Strawberry","Litchi"]
+cart = ["Milk","Chocolate","Shampoo"]
 
+#* Length
+print(len(fruits))
 
+#* Accessing Lists
+print(fruits[0])
+
+#* Insert 
+fruits.insert(0,"Grapes")
+
+#* Append 
+fruits.append("Kiwi")
+
+#* Extend 
+fruits.extend(cart)
+
+#* Remove
+fruits.remove("Shampoo")
+
+#* Pop
+fruits.pop(0)
+fruits.pop()
+
+#* Del
+del fruits[2]
+#! del fruits   #Removes whole list
+
+#* Clear
+# fruits.clear()
+
+#* Sort
+fruits.sort()
+fruits.sort(reverse=True)
+
+#* Reverse
+fruits.reverse()
+
+#* Count
+elementCount = fruits.count("Apple")
+
+#* Index 
+elementIndex = fruits.index("Litchi")
+
+print(fruits)
+print(elementCount)
+print(elementIndex)

13af0f2f34df8a52f342b88c003c01ac46139984
Yug <yugjain1646@gmail.com>
2026-08-30 22:30:30 +0530

    Modules

---
+++ chapter_7.py
@@ -0,0 +1,15 @@
+
+#! ----- Chapter 7 - Modules -----
+
+import circleOperations
+
+#? Built in modules
+
+import math
+print(math.pi)
+
+#? User define modules
+area = circleOperations.circleArea(5)
+circumference = circleOperations.circleCircumference(5)
+print("Area:", area)
+print("Circumference:", circumference)
---
+++ circleOperations.py
@@ -0,0 +1,6 @@
+
+def circleArea(radius):
+    return 3.14 *radius
+
+def circleCircumference(radius):
+    return 2 * 3.14 * radius

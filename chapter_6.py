8f2f8011a48247e4c7d12de7d43b19c18e406f06
Yug <yugjain1646@gmail.com>
2026-08-30 17:32:54 +0530

    Function & Types of Function

--- chapter_6.py
+++ chapter_6.py
@@ -1,2 +1,37 @@
 
 #! ----- Chapter 6 - Functions -----
+
+
+#? Normal Function
+def message():
+    print("Hello World !")
+message()
+
+#? Function with Arguments
+def happy_birthday(personName,years):
+    print(f"Happy birthday {personName}")
+    print(f"You are {years} old")
+happy_birthday("Yug",21)
+
+#? Return Function
+def add(x,y):
+    return x + y
+result = add(5,6)
+print(result)
+
+
+#? Default Arguments
+def stud_details(stud_name, branch = "CS", dept = "B.Tech"):
+    return {
+        "Student Name" : stud_name,
+        "Branch" : branch,
+        "Department" : dept
+    }
+print(stud_details("Tony"))
+
+
+#? Keyword Arguments
+def greetings(msg,firstName,middleName,lastName):
+    return msg + " " + firstName + " " + middleName + " " + lastName
+greetUser = greetings("Hello", firstName = "Tony", lastName = "Stark", middleName = "Howard")
+print(greetUser)
